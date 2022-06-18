dd421da636109d2c1b16e77aa689fff684c2c1a3
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-06-18 12:37:35 -0300

    sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { Header2Component } from './template/header2/header2.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Header2Component } from './template/header2/header2.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    Header2Component
+    Header2Component,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
